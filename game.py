659dedcb41cde9ad6fb020678e1341298425051e
ianboss123 <93549842+ianboss123@users.noreply.github.com>
2021-11-02 11:16:40 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,15 @@
 
+from ConnectNGame.src import board
+
+
 class Game(object):
+    def __init__(self, board_array: board.Board, pieces_to_win: int):
+        self.board_array = board_array
+        self.players = []
+        self.board = board
+        self.pieces_to_win = pieces_to_win
+
+    def create_player(self, player_num):
+        enter_player = input(f"Player {player_num} enter your name")
+        self.players.append(enter_player)    
     ...
