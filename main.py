e38992b5fc461b34fbe8b511691f17351e889351
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-02 18:57:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from ConnectNGame.src.game import Game
 from ConnectNGame.src.board import Board
 from ConnectNGame.src.player import Player
 def main() -> None:
-    Game.create_player()
-    Game.create_player()
+    game_master=Game()
+    game_master.create_player()
+    game_master.create_player()
+    playing=True
+    while playing:
+        for x in game_master.Player_instants:
+
+
+    ...  # program execution begins here
+
+
 if __name__ == '__main__':
     main()
