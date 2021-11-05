2c822c9caeb1e322556912fb51f25b4e77cdae5c
ianboss123 <93549842+ianboss123@users.noreply.github.com>
2021-11-05 16:50:31 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import sys
+
 from ConnectNGame.src.board import Board
 from ConnectNGame.src.player import Player
 from ConnectNGame.src.config import Config
@@ -6,25 +8,28 @@ from typing import List, Tuple
 
 class Game(object):
     def __init__(self, game_config: Config):
-        self.players: List[Tuple[str, str]] = []
-        self.Player_instants: List[Player] = []
+        self.players: List[Tuple[str, str, int]] = []
         self.board: Board = Board.build_board_from_config(game_config)
+        self.Player_instants: List[Player] = []
         self.player_num = 0
 
     def create_player(self):
         self.player_num += 1
         player_name = self.check_player_name(self.player_num)
         player_piece = self.check_player_piece(self.player_num)
-        self.players.append((player_name, player_piece))
-        globals()[player_name] = Board(player_name, player_piece, self.player_num)
+        self.players.append((player_name, player_piece, self.player_num))
+        globals()[player_name] = Player(player_name, player_piece, self.player_num)
         self.Player_instants.append(globals()[player_name])
 
     def check_player_name(self, player_num):
         x = [t[0] for t in self.players]
+        x = [t.lower() for t in x]
+        print(x)
+
         while True:
-            player_name = input(f"Player {player_num} enter your name")
-            player_name = player_name.lower()
-            if len(player_name) == 0 or player_name == " ":
+            player_name = input(f"Player {player_num} enter your name: ")
+            player_name_lower = player_name.lower()
+            if len(player_name_lower) == 0 or player_name == " ":
                 print("Your name cannot be the empty string or whitespace")
                 continue
             elif player_name in x:
@@ -35,12 +40,13 @@ class Game(object):
     def check_player_piece(self, player_num):
         x = [t[1] for t in self.players]
         while True:
-            piece = input(f"Player {player_num} enter your piece")
+            piece = input(f"Player {player_num} enter your piece: ")
             if len(piece) == 0 or piece == " ":
                 print("Your piece cannot be the empty string or whitespace")
                 continue
             elif len(piece) > 1:
                 print(f'{piece} is not a single character. Your piece can only be a single character.')
+                continue
             elif piece == self.board.blank_character:
                 print('Your piece cannot be the same as the blank character.')
                 continue
@@ -49,21 +55,37 @@ class Game(object):
                 print(f'You cannot use {piece} for your piece as {self.players[pos][0]} is already using it.')
                 continue
             return piece
-    
+
     def play(self):
         self.create_player()
         self.create_player()
         print(self.board)
 
+        while True:
+            for x in self.Player_instants:
+                play_col = int(input(f"{x.name} please enter the column you want to play in: "))
+                self.board.drop_piece_into_column(play_col, x.piece)
+                print(self.board)
+            # game_master.board.fill_spot(play_col, x.piece)
+#             playing = game_master.win_check(x.piece)
+#             winner = x.name
+#     print(f"{winner} won the game!")
+#
+
+
+
+
     def win_check(self, piece):
-        board_list=self.board
-        longest_vect=0
+        board_list = self.board
+        longest_vect = 0
+
         def check_next(col, row, col_chng, row_chng):
             next_chr = board_list[col + col_chng][row + row_chng]
             if next_chr == piece:
                 return 1 + check_next(col, row, col_chng, row_chng)
             else:
                 return 0
+
         for col in range(self.board.num_columns):
             for row in range(self.board.num_rows):
                 if piece == self.board.contents[col][row]:
@@ -71,6 +93,11 @@ class Game(object):
                     tor_right_vect = check_next(col, row, 1, 1)
                     right_vect = check_next(col, row, 1, 0)
                     bottom_right_vect = check_next(col, row, 0, 1)
-                    longest_vect=max(up_vect,tor_right_vect,right_vect,bottom_right_vect,longest_vect)
-        if longest_vect>=self.num_pieces_to_win:
+                    longest_vect = max(up_vect, tor_right_vect, right_vect, bottom_right_vect, longest_vect)
+        if longest_vect >= self.board.pieces_to_win:
             return False
+
+
+
+
+
