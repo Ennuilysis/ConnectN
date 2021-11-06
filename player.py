cf5ac5999254e87444bab404c414211d975b3f8b
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-06 14:53:25 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 
 class Player(object):
-       def __init__(self, name, piece, number: int):
+       def __init__(self, name: str, piece: str, number: int):
         self.num: int = number
         self.name: str = name
         self.piece: str = piece
