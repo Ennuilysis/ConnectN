c10c0fe5117e568464a5d8add0dc0858925d6c5c
ianboss123 <93549842+ianboss123@users.noreply.github.com>
2021-11-05 20:45:15 -0700

    Update board.py

--- board.py
+++ board.py
@@ -36,8 +36,8 @@ class Board(object):
 
         return final_result
 
-#     def fill_spot(self, row: int, column: int, character: str):
-#         self.contents[row][column] = character
+    def fill_spot(self, row: int, column: int, character: str):
+        self.contents[row][column] = character
 
     def drop_piece_into_column(self, column, piece):
         for row in reversed(self.contents):
@@ -45,14 +45,10 @@ class Board(object):
                 row[column] = piece
                 break
 
-
-
-
-
-
     @staticmethod
     def build_board_from_config(game_config) -> "Board":
 
         return Board(game_config.num_rows, game_config.num_columns, game_config.blank_character,
-                 game_config.num_pieces_to_win)
+                     game_config.num_pieces_to_win)
+
 
