c604514f64b80960905d3465d9eb78613094359a
Ennuilysis <78125306+Ennuilysis@users.noreply.github.com>
2021-11-06 14:52:37 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,7 @@
-import sys
-
 from ConnectNGame.src.board import Board
 from ConnectNGame.src.player import Player
 from ConnectNGame.src.config import Config
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 
 class Game(object):
@@ -13,30 +11,30 @@ class Game(object):
         self.Player_instants: List[Player] = []
         self.player_num = 0
 
-    def create_player(self):
+    def create_player(self) -> None:
         self.player_num += 1
-        player_name = self.check_player_name(self.player_num)
-        player_piece = self.check_player_piece(self.player_num)
+        player_name : str= self.check_player_name(self.player_num)
+        player_piece : str = self.check_player_piece(self.player_num)
         self.players.append((player_name, player_piece, self.player_num))
         globals()[player_name] = Player(player_name, player_piece, self.player_num)
         self.Player_instants.append(globals()[player_name])
 
-    def check_player_name(self, player_num):
+    def check_player_name(self, player_num) -> str:
         x = [t[0] for t in self.players]
         x = [t.lower() for t in x]
 
         while True:
             player_name = input(f"Player {player_num} enter your name: ")
             player_name_lower = player_name.lower()
-            if len(player_name_lower) == 0 or player_name == " ":
+            if len(player_name) == 0 or player_name == " ":
                 print("Your name cannot be the empty string or whitespace")
                 continue
-            elif player_name in x:
+            elif player_name_lower in x:
                 print(f'You cannot use {player_name} for your name as someone else is already using it.')
                 continue
             return player_name
 
-    def check_player_piece(self, player_num):
+    def check_player_piece(self, player_num) -> str:
         x = [t[1] for t in self.players]
         while True:
             piece = input(f"Player {player_num} enter your piece: ")
@@ -60,33 +58,47 @@ class Game(object):
             self.create_player()
             self.create_player()
             print(self.board)
-
             while True:
                 for x in self.Player_instants:
-                    play_col = int(input(f"{x.name} please enter the column you want to play in: "))
+                    play_col : int= self.play_check(x.name) # type: ignore
                     self.board.drop_piece_into_column(play_col, x.piece)
                     print(self.board)
                     win = self.win_check(x.piece)
                     if win:
                         print(f"{x.name} won the game!")
-                        exit()
+                        quit()
 
-
-
-
-
-
-    def win_check(self, piece):
-        board_list = self.board.contents
-        longest_vect = 0
-
-        def check_next(row, col, row_chng, col_chng):
+    def play_check(self, player) -> Union[str]:
+        col = self.board.num_columns
+        while True:
             try:
-                next_chr = board_list[row+row_chng][col+col_chng]
+                pos = input(f"{player}, please enter the column you want to play in: ")
+                pos = int(pos)
+            except:
+                print(f'{player}, column needs to be an integer. {pos} is not an integer. ') # type: ignore
+                continue
+            if pos > col - 1 or pos < 0:
+                print(f'Your column needs to be between 0 and {col - 1} but is actually {pos}.') # type: ignore
+                continue
+            x : List[str] = [t[pos] for t in self.board.contents]
+            if self.board.blank_character not in x:
+                print(f'You cannot play in {pos} because it is full.')
+                continue
+            break
+        return pos
+
+    def win_check(self, piece) -> bool:
+        board_list: List[List[str]] = self.board.contents
+        longest_vect = 0
+        if self.board.blank_character not in self.board.contents[0]:
+            print("Tie Game.")
+            quit()
+
+        def check_next(row: int, col: int, row_chng: int, col_chng: int) -> int:
+            try:
+                next_chr: str = board_list[row + row_chng][col + col_chng]
             except IndexError:
-
                 return 0
-
 
             if next_chr == piece:
                 return 1 + check_next(row + row_chng, col + col_chng, row_chng, col_chng)
@@ -96,20 +108,11 @@ class Game(object):
         for col in range(self.board.num_columns):
             for row in range(self.board.num_rows):
                 if piece == self.board.contents[row][col]:
-                    up_vect = check_next(row, col, 1, 0)
-                    tor_right_vect = check_next(row, col, 1, 1)
-                    right_vect = check_next(row, col, 0, 1)
-                    bottom_right_vect = check_next(row, col, 1, -1)
-                    longest_vect = max(up_vect, tor_right_vect, right_vect, bottom_right_vect, longest_vect)
+                    up_vect: int = check_next(row, col, 1, 0)
+                    top_right_vect: int = check_next(row, col, 1, 1)
+                    right_vect: int = check_next(row, col, 0, 1)
+                    bottom_right_vect: int = check_next(row, col, 1, -1)
+                    longest_vect: int = max(up_vect, top_right_vect, right_vect, bottom_right_vect, longest_vect)
         if longest_vect >= self.board.pieces_to_win - 1:
             return True
-
-
-
-
-
-
-
-
-
-
+        return False
